46120ca4d6acf5e00fad3ae81454610d26d5db70
BIOLYTIC\ut.duong <ut.duong@biolytic.com>
2017-05-10 11:44:25 -0700

    Add project page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about', (req,res) => {
     });
 });
 
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'Portfolio Page',
+        welcomeMessage: "My Projects"
+    });
+});
+
 app.get('/bad', (req,res) => {
     res.send({
         errorMessage: 'Unable to handle request!'
